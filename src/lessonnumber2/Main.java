d0af5d5303a95689c4a34855ed73419bbb84b71c
katetimonova <kateimonova@gmail.com>
2023-01-21 20:11:16 +0200

    Initial commit

---
+++ src/lessonnumber2/Main.java
@@ -0,0 +1,12 @@
+package lessonnumber2;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 38;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a/b);
+        System.out.println(a*b);
+    }
+}
