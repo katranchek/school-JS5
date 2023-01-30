c2ba00424bfd54f042283da6f3d72c3e63a5db2e
katetimonova <kateimonova@gmail.com>
2023-01-30 23:29:03 +0200

    lesson5

--- src/lessonnumber5/Main.java
+++ src/lessonnumber5/Main.java
@@ -4,10 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         int number = 93;
+        if (number > 12)
         System.out.println(number + "> 12");
         int number2 = 10;
-        System.out.println(number2 + "<11");
+        if (number2 < 12)
+        System.out.println(number2 + "<12");
         System.out.println(number2 + "is even");
     }
-
 }
