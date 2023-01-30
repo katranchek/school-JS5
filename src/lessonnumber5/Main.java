164d144d83fed4a5dff5a9e5bf54f7867ef7f648
katetimonova <kateimonova@gmail.com>
2023-01-30 20:20:07 +0200

    lesson5

---
+++ src/lessonnumber5/Main.java
@@ -0,0 +1,13 @@
+package lessonnumber5;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int number = 93;
+        System.out.println(number + "> 12");
+        int number2 = 10;
+        System.out.println(number2 + "<11");
+        System.out.println(number2 + "is even");
+    }
+
+}
