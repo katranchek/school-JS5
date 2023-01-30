2b588b2d257aeec6c3f12172f2615322caa5f445
katetimonova <kateimonova@gmail.com>
2023-01-31 00:04:32 +0200

    lesson5

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int number = 93;
+        if (number > 12)
+        System.out.println(number + "> 12");
+        int number2 = 10;
+        if (number2 < 12)
+            System.out.println(number + "< 12");
+        int number3 = 30;
+        if (number3 % 2== 0){
+            System.out.println(number3 + "is even");
+        }
+    }
+}
